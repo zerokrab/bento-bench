18ca923f4769b33d1d1f3dd107dc2e8d2ab9b065
mintybasil <163682877+mintybasil@users.noreply.github.com>
2025-12-01 17:04:24 -0500

    Refactor bench command

---
+++ crates/bencher/src/commands/refactor/bench.rs
@@ -0,0 +1,138 @@
+use crate::ProverConfig;
+use crate::commands::refactor::manifest::load_manifest;
+use crate::prove::prove_bonsai;
+use anyhow::Result;
+use bonsai_sdk::non_blocking::Client as BonsaiClient;
+use clap::Args;
+use serde::Serialize;
+use std::path::PathBuf;
+use tabled::settings::{Rotate, Settings};
+use tabled::{Table, Tabled};
+use tokio::fs::write;
+
+#[derive(Args, Clone, Debug)]
+pub struct BenchArgs {
+    /// Path to manifest file
+    #[clap(long = "manifest", default_value = "./manifest.json")]
+    manifest_path: PathBuf,
+    /// Directory to load images/inputs from
+    #[clap(long)]
+    data_dir: PathBuf,
+    /// Execute only (no prove)
+    #[clap(long, default_value_t = false)]
+    exec_only: bool,
+    /// Output results to json file
+    #[clap(long)]
+    json: Option<PathBuf>,
+
+    #[clap(flatten, next_help_heading = "Prover")]
+    pub prover_config: ProverConfig,
+}
+
+#[derive(Tabled, Serialize)]
+pub struct BenchResult {
+    pub description: String,
+    pub segments: u64,
+    /// Total cycles run within guest
+    pub total_cycles: u64,
+    /// User cycles run within guest, slightly below total overhead cycles
+    pub cycles: u64,
+    pub exec_time_secs: f64,
+    pub prove_time_secs: f64,
+    pub exec_khz: f64,
+    pub prove_khz: f64,
+}
+
+impl BenchArgs {
+    /// Run the datasheet generate command
+    pub async fn run(&self) -> Result<()> {
+        let manifest = load_manifest(&self.manifest_path)?;
+
+        self.prover_config
+            .proving_backend
+            .configure_proving_backend();
+
+        // Currently only support Bento, not default prover
+        let prover: BonsaiClient = BonsaiClient::from_env(risc0_zkvm::VERSION)?;
+
+        let data_dir = self.data_dir.clone();
+        let images_dir = data_dir.join("images");
+        let inputs_dir = data_dir.join("inputs");
+
+        let mut res = Vec::new();
+        let mut count = 1;
+        let total = manifest.entries.len();
+        for entry in manifest.entries.iter() {
+            tracing::info!("Running benchmark {count} of {total}...");
+            let image_id = entry.image_id.clone().expect("image id missing");
+            let input_id = entry.input_id.clone().expect("input id missing");
+
+            let elf = std::fs::read(images_dir.join(format!("{}.elf", image_id)))?;
+
+            let input = std::fs::read(inputs_dir.join(format!("{}.input", input_id)))?;
+
+            let (session_stats, exec_elapsed_secs) = prove_bonsai(
+                prover.clone(),
+                image_id.clone(),
+                elf.clone(),
+                input.clone(),
+                true,
+            )
+            .await?;
+
+            let prove_elapsed_secs = if self.exec_only {
+                0.0
+            } else {
+                prove_bonsai(
+                    prover.clone(),
+                    image_id.clone(),
+                    elf.clone(),
+                    input.clone(),
+                    self.exec_only,
+                )
+                .await?
+                .1
+            };
+
+            let exec_khz = if exec_elapsed_secs > 0.0 {
+                session_stats.total_cycles as f64 / exec_elapsed_secs / 1000.0
+            } else {
+                0.0
+            };
+            let prove_khz = if prove_elapsed_secs > 0.0 {
+                session_stats.total_cycles as f64 / prove_elapsed_secs / 1000.0
+            } else {
+                0.0
+            };
+
+            let bench_result = BenchResult {
+                description: entry.description.clone(),
+                segments: session_stats.segments as u64,
+                total_cycles: session_stats.total_cycles,
+                cycles: session_stats.cycles,
+                exec_time_secs: exec_elapsed_secs,
+                prove_time_secs: prove_elapsed_secs,
+                exec_khz,
+                prove_khz,
+            };
+
+            print_bench_result(&bench_result);
+
+            res.push(bench_result);
+            count += 1;
+        }
+
+        if let Some(out_path) = self.json.clone() {
+            let out_str = serde_json::to_string_pretty(&res)?;
+            write(&out_path, &out_str).await?;
+            tracing::info!("Wrote results to {:?}", out_path);
+        }
+
+        Ok(())
+    }
+}
+
+fn print_bench_result(bench_result: &BenchResult) {
+    let table_config = Settings::default().with(Rotate::Left);
+    println!("{}", Table::new(vec![bench_result]).with(table_config));
+}
